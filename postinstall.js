051b4466b0a779a6b7f7b1f748a69f16cb6d517f
Evan Bacon <baconbrix@gmail.com>
2018-08-08 13:12:28 -0700

    updated rimraf script

---
+++ postinstall.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var rimraf = require('rimraf');
+rimraf('../../gl-matrix/.babelrc', err => {
+  if (err != null) {
+    console.log(err);
+  } else {
+    console.log('completed');
+  }
+});
